445a98b8ec2005ea86aeb1635e97d060f8f1492c
MarceloMilbradt <milbradthz@gmail.com>
2021-09-15 15:49:47 -0300

    Create NHibernateContractResolver.cs

---
+++ Repository/NHibernateContractResolver.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovosColaboradores.Repository
+{
+    public class NHibernateContractResolver : DefaultContractResolver
+    {
+        protected override JsonContract CreateContract(Type objectType)
+        {
+            if (typeof(NHibernate.Proxy.INHibernateProxy).IsAssignableFrom(objectType))
+                return base.CreateContract(objectType.BaseType);
+            else
+                return base.CreateContract(objectType);
+        }
+    }
+}
+//JsonConvert.SerializeObject(pessoas, new JsonSerializerSettings { ContractResolver = new NHibernateContractResolver()});
